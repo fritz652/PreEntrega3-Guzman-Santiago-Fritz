ad58da9d44862395701b352c921d41b3e6d922c1
Fritz <fritz.developer10@gmail.com>
2023-09-01 19:33:51 -0500

    ultiam actualizacion

--- app/cart.js
+++ app/cart.js
@@ -1,38 +1,63 @@
-let dataStorage = JSON.parse(localStorage.getItem("formularios"));  // esto es como si fuera una cartera de neuvos clientes
-console.log(dataStorage);
+let dataStorage = JSON.parse(localStorage.getItem("formularios"));
 
 function openCart() {
     const cart = document.querySelector("#cartButton");
-    const container = document.createElement("div"); // Crear un contenedor para los elementos
+    const container = document.createElement("div");
     container.className = "newCard";
 
-    dataStorage.forEach(element => {
-        const content = document.createElement("div"); // Crear un elemento para cada elemento en dataStorage
-        content.classList.add("element");
-        content.innerHTML = `
+    // Función para actualizar la vista
+    function actualizarVista() {
+        container.innerHTML = ''; // Limpiar el contenedor
+        dataStorage.forEach(element => {
+            const content = document.createElement("div");
+            content.classList.add("element");
+            content.innerHTML = `
                 <div>
                     <p>Solicitud Nro: ${element.id}</p>
                     <p>Nombre : ${element.nombre}</p>
                     <p>Monto: ${element.monto}</p>
                     <p>Meses: ${element.meses}</p>
                 </div>
-                <divn><span class="trash">...</span></divn>
-        `;
-        content.style.display= `flex`
-        content.style.margin= `30px`
+                <div><span class="trash">...</span></div>
+            `;
+            content.style.display = `flex`;
+            content.style.margin = `30px`;            
 
-        container.appendChild(content); // Agregar el elemento actual al contenedor
-    });
+            container.appendChild(content);
+
+            const deleteCard = content.querySelector(".trash");
+            deleteCard.addEventListener('click', () => {
+                deleteCardSolicitud(element.id);
+            });
+        });
+    }
+
+    actualizarVista(); // Llamar a la función para mostrar inicialmente los elementos
 
     cart.addEventListener("click", () => {
-        // Toggle para mostrar u ocultar el contenido
         if (document.body.contains(container)) {
             document.body.removeChild(container);
         } else {
             document.body.appendChild(container);
         }
-        
     });
+
+    // Función para eliminar un elemento y actualizar la vista
+    const deleteCardSolicitud = (id) => {
+        const foundId = dataStorage.findIndex((element) => element.id === id);
+        if (foundId !== -1) {
+            dataStorage.splice(foundId, 1);
+
+            // Actualizar el localStorage después de eliminar un elemento
+            localStorage.setItem("formularios", JSON.stringify(dataStorage));
+
+            // Volver a mostrar los formularios actualizados
+            actualizarVista();
+
+        }
+    };
 }
 
 openCart();
+
+
